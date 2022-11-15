138d394f58db8569bf0cb30a728635e4921522a5
SimonWeilCatlinGabel <113215817+WeilSimon@users.noreply.github.com>
2022-11-15 09:49:18 -0800

    Elevator and updated constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,7 +14,18 @@ package frc.robot;
  */
 public final class Constants {
 
+    public static final class DrivetrainConstants{
+        public static final int LEFT_FRONT = 1;
+        public static final int LEFT_BACK = 2;
+        public static final int RIGHT_FRONT = 3;
+        public static final int RIGHT_BACK = 4;
+    }
+
     public static final class IntakeConstants{
         public static final int VICTOR = 5;
+    }
+
+    public static final class ElevatorConstants{
+        public static final int FALCON = 6;
     }
 }
